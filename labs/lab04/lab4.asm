8853054dc1b2aa46910c8b0767ef79a0874424a9
olesyachernyatjeva <0208olesya@gmail.com>
2023-11-10 18:45:43 +0300

    feat(main): add files lab-5

--- labs/lab04/lab4.asm
+++ labs/lab04/lab4.asm
@@ -1,6 +1,6 @@
 ; hello.asm
 SECTION .data ; Начало секции данных
-hello: DB 'Hello world!',10 ; 'Hello world!' плюс
+hello: DB 'Чернятьева Олеся!',10 ; 'Hello world!' плюс
 ; символ перевода строки
 helloLen: EQU $-hello ; Длина строки hello
 SECTION .text ; Начало секции кода
